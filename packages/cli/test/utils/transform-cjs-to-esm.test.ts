0bd9eba5a17bc7ba89985fadbbf3521cd8fdc440
aleksey-hoffman <61761672+aleksey-hoffman@users.noreply.github.com>
2026-01-11 01:52:51 +0300

    Dev: prerelease chores

--- packages/cli/test/utils/transform-cjs-to-esm.test.ts
+++ packages/cli/test/utils/transform-cjs-to-esm.test.ts
@@ -1,7 +1,7 @@
 import { expect, it } from 'vitest';
 import { transformCJSToESM } from '../../src/utils/transformers/transform-cjs-to-esm';
-import { TAILWIND_CONFIG_TEMPLATE } from '~/packages/shared/templates/tailwind-config';
+import { TAILWIND_CONFIG_JS_TEMPLATE } from '~/packages/shared/templates/tailwind-config';
 
 it('handle tailwind config template correctly', () => {
-  expect(transformCJSToESM('.mjs', TAILWIND_CONFIG_TEMPLATE)).toMatchSnapshot();
+  expect(transformCJSToESM('.mjs', TAILWIND_CONFIG_JS_TEMPLATE)).toMatchSnapshot();
 });
